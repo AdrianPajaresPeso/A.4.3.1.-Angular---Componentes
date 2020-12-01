d3159762b70ab3b4535c870c6f33f113f7ab7eb2
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-12-01 12:47:39 +0100

    develop / punto C hecho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,14 @@ import { ListadoComponent } from './listado/listado.component';
 import {FormsModule} from '@angular/forms';
 import { MisclientesComponent } from './misclientes/misclientes.component';
 import { CondicionalesComponent } from './condicionales/condicionales.component';
+import { CorreoComponent } from './components/correo/correo.component';
 @NgModule({
   declarations: [
     AppComponent,
     ListadoComponent,
     MisclientesComponent,
-    CondicionalesComponent
+    CondicionalesComponent,
+    CorreoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/correo/correo.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-correo',
+  templateUrl: './correo.component.html',
+  styleUrls: ['./correo.component.css']
+})
+export class CorreoComponent implements OnInit {
+
+  correo: any;
+  constructor() {
+    this.correo = {
+      titulo: 'titulo del email',
+      cuerpo: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum cumque qui tempore eligendi error doloribus ipsum harum a, modi alias, labore quis eveniet et voluptates quidem perspiciatis similique eaque totam.',
+      emisor: 'correoEmisor@openWebinar.inv',
+      destinatario: 'correoReceptor@openWebinar.inv'
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+}
