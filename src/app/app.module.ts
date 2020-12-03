159806e33ff94cee2e746a3f8ea1109206ad77e7
Adrian Pajares Peso <dam08.2020.jesuitas@gmail.com>
2020-12-03 08:46:57 +0100

    reorganizado y finalizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ListadoComponent } from './listado/listado.component';
+import { ListadoComponent } from './components/listado/listado.component';
 
 import {FormsModule} from '@angular/forms';
-import { MisclientesComponent } from './misclientes/misclientes.component';
-import { CondicionalesComponent } from './condicionales/condicionales.component';
+import { MisclientesComponent } from './components/misclientes/misclientes.component';
+import { CondicionalesComponent } from './components/condicionales/condicionales.component';
 import { CorreoComponent } from './components/correo/correo.component';
-import { ListacorreosComponent } from './listacorreos/listacorreos.component';
+import { ListacorreosComponent } from './components/listacorreos/listacorreos.component';
 @NgModule({
   declarations: [
     AppComponent,
